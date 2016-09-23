4f3f275348d7addd9b9e6f4aaba887fe659f43fa
Michael Remijan <mjremijan@yahoo.com>
2016-09-23 08:44:28 -0500

    first commit.

---
+++ jaxrs-inject/src/main/java/org/thoth/jaxrs/inject/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package org.thoth.jaxrs.inject;
+
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Michael Remijan mjremijan@yahoo.com @mjremijan
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+}
